eeae23b245ab1a92df6ddda7a713201437051efe
Adrian Hagjija <adrianhagjija@hotmail.com>
2021-01-29 11:30:36 +0100

    add vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
+import VueRouter from 'vue-router'
 import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
+Vue.use(VueRouter)
 
 new Vue({
   vuetify,
