696af6132a56e5354075d4a10faeec21e4dfd750
Axel Duret <axelfaya@gmail.com>
2021-01-29 13:51:42 +0100

    reinit project to fix bug

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,41 @@
 <template>
-  <div id="app"></div>
+  <v-app>
+    <v-main
+      ><v-row
+        ><v-col>
+          <router-link class="text-decoration-none" to="/"
+            ><v-btn block text class=" white--text" color="deep-purple"
+              >Home</v-btn
+            ></router-link
+          ></v-col
+        >
+        <v-col>
+          <router-link class="text-decoration-none" to="/about"
+            ><v-btn block text class=" white--text" color="deep-purple"
+              >About Us</v-btn
+            ></router-link
+          ></v-col
+        >
+        <v-col>
+          <router-link class="text-decoration-none" to="/video"
+            ><v-btn block text class=" white--text" color="deep-purple"
+              >Video</v-btn
+            ></router-link
+          ></v-col
+        ></v-row
+      >
+      <hr color="black" />
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 export default {
   name: "App",
-  components: {},
+
+  data: () => ({
+    //
+  }),
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
