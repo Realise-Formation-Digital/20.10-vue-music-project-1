6c602219d90d6d7207e26b95e658de0c7724a71a
Marco Tribuzio <marco.tribuz@gmail.com>
2021-02-02 16:47:40 +0100

    bugfix home

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <v-app>
-    <v-main
-      ><v-row
-        ><v-col>
-          <router-link class="text-decoration-none" to="/"
-            ><v-btn block text class=" white--text" color="deep-purple"
-              >Home</v-btn
-            ></router-link
-          ></v-col
-        >
+    <v-main>
+      <v-row>
+        <v-col>
+          <router-link class="text-decoration-none" to="/">
+            <v-btn block text class=" white--text" color="deep-purple">Home</v-btn>
+          </router-link>
+        </v-col>
         <v-col>
           <router-link class="text-decoration-none" to="/about">
             <v-btn block text class=" white--text" color="deep-purple">About Us</v-btn>
           </router-link>
         </v-col>
         <v-col>
-          <router-link class="text-decoration-none" to="/video"
-            ><v-btn block text class=" white--text" color="deep-purple"
-              >Video</v-btn
-            ></router-link
-          ></v-col
-        ></v-row
-      >
-      <hr color="black" />
+          <router-link class="text-decoration-none" to="/video">
+            <v-btn block text class=" white--text" color="deep-purple">Video</v-btn>
+          </router-link>
+        </v-col>
+      </v-row>
+      <hr color="black"/>
 
 
       <router-view>
