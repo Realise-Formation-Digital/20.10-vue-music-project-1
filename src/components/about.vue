22f9168f1261091ca78eeba5f23b3d9c28a62417
Adrian Hagjija <adrianhagjija@hotmail.com>
2021-02-03 15:23:25 +0100

    about page fixed (everything in the center now) and link from home to video

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,26 +1,38 @@
 <template>
-  <div>
+
+
+  <v-row>
+    <v-col cols="1"></v-col>
+    <v-col cols="10">
       <v-card class=text-center>
-      <v-card-title class="justify-center" id="title">Qui sommes nous ?</v-card-title>
-      <p>Une team d'amoureux de la mayonnaise réuni autour d'une seul idée : Réunir leurs passion pour la vidéo et la mayo en un seul projet ! <br>
-      Axel, Adrian, Gaby, Hugo et Michael. 5 noms qui ne vous disent rien pour l'instant mais qui risque de vous parler dans quelques années. <br>
-      </p>
+        <v-card-title class="justify-center" id="title">Qui sommes nous ?</v-card-title>
+        <p>Une team d'amoureux de la mayonnaise réuni autour d'une seul idée : Réunir leurs passion pour la vidéo et la mayo en un seul projet ! <br>
+          Axel, Adrian, Gaby, Hugo et Michael. 5 noms qui ne vous disent rien pour l'instant mais qui risque de vous parler dans quelques années. <br>
+        </p>
       </v-card>
       <h1>Formulaire de contact</h1>
       <Form></Form>
       <h1 id="where"> Où sommes nous ?</h1>
-      <a href="https://www.google.com/maps/place/Rue+Viguet+8,+1227+Gen%C3%A8ve,+Suisse/@46.192996,6.1266046,17z/data=!3m1!4b1!4m5!3m4!1s0x478c7b2e1299827b:0x6058f3a4660b6a0b!8m2!3d46.192996!4d6.1287933">
-        <v-img id="map"
-          contain
-          alt="map MayoTube"
-          height="900"
-          width="900"
-          src="../assets/adresseMayoTube.png"
-      ></v-img>
-      </a>
-  </div>
+      <v-row>
+        <v-col cols="1"></v-col>
+        <v-col cols="10">
+          <a href="https://www.google.com/maps/place/Rue+Viguet+8,+1227+Gen%C3%A8ve,+Suisse/@46.192996,6.1266046,17z/data=!3m1!4b1!4m5!3m4!1s0x478c7b2e1299827b:0x6058f3a4660b6a0b!8m2!3d46.192996!4d6.1287933">
+            <v-img id="map"
+                   contain
+                   alt="map MayoTube"
+                   src="../assets/adresseMayoTube.png"
+            ></v-img>
+          </a>
+        </v-col>
+        <v-col cols="1"></v-col>
+      </v-row>
 
 
+
+
+    </v-col>
+    <v-col cols="1"></v-col>
+  </v-row>
 </template>
 
 <script>
@@ -53,9 +65,5 @@ export default {
   }
   #where{
     padding-top: 10px;
-  }
-  #map{
-    display: block;
-    margin-left: 260px;
   }
 </style>
